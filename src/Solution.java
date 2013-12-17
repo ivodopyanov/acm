91960e31e1153c480a1955b56f435d6ad4236377
Igor Vodopyanov <ivodopyanov@naumen.ru>
2013-12-17 19:11:17 +0600

    Проходит все тесты, но валится на тимусе

--- src/Solution.java
+++ src/Solution.java
@@ -1,5 +1,5 @@
-import java.io.PrintWriter;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 /**
  * 
@@ -12,11 +12,304 @@ import java.util.Scanner;
  */
 public class Solution
 {
-    public static void main(String[] args)
+    public static Point findAnswer(boolean[][] passable, Point start, Point end)
     {
-        Scanner in = new Scanner(System.in);
-        PrintWriter out = new PrintWriter(System.out);
+        AStarMap map = new AStarMap(passable);
+        int bestLength = 0;
+        Point result = null;
+        LinkedList<Point> path = map.findPath(start, end);
+        path.removeLast();
+        for (Point point : path)
+        {
+            map.reset();
+            passable[point.getX()][point.getY()] = false;
+            List<Point> updatedPath = map.findPath(start, end);
+            if (updatedPath == null)
+            {
+                return point;
+            }
+            if (updatedPath.size() > bestLength)
+            {
+                bestLength = updatedPath.size();
+                result = point;
+            }
+            passable[point.getX()][point.getY()] = true;
 
-        out.flush();
+        }
+        return result;
     }
+
+    public static void main(String[] args) throws IOException
+    {
+
+        boolean oj = System.getProperty("ONLINE_JUDGE") != null;
+        Reader reader = oj ? new InputStreamReader(System.in) : new FileReader("input.txt");
+        Writer writer = oj ? new OutputStreamWriter(System.out) : new FileWriter("output.txt");
+        Scanner in = new Scanner(reader);
+        PrintWriter out = new PrintWriter(writer);
+        int height = in.nextInt();
+        int width = in.nextInt();
+        Point start = new Point(in.nextInt() - 1, in.nextInt() - 1);
+        Point end = new Point(in.nextInt() - 1, in.nextInt() - 1);
+        boolean[][] passable = new boolean[width][height];
+        for (int y = 0; y < height; y++)
+        {
+            String s = in.next();
+            for (int x = 0; x < width; x++)
+            {
+                passable[x][y] = s.charAt(x) == '.';
+            }
+        }
+        in.close();
+
+        Point result = findAnswer(passable, start, end);
+        out.write(String.format("%d %d", result.getX() + 1, result.getY() + 1));
+        out.flush();
+        out.close();
+    }
+}
+
+class AStarMap
+{
+    private static final Point[] DISPS = new Point[] { new Point(-1, 0), new Point(1, 0), new Point(0, -1),
+            new Point(0, 1) };
+    private final boolean[][] passable;
+    private AStarNode[][] nodes;
+    private final Point dims;
+
+    public AStarMap(boolean[][] passable)
+    {
+        this.passable = passable;
+        this.dims = new Point(passable.length, passable[0].length);
+        nodes = new AStarNode[dims.getX()][dims.getY()];
+    }
+
+    public LinkedList<Point> findPath(Point start, Point end)
+    {
+        TreeSet<AStarNode> visitedNodes = new TreeSet<AStarNode>(new AStarNode.AStarNodeComparator());
+        LinkedList<AStarNode> nodesToVisit = new LinkedList<AStarNode>();
+        nodesToVisit.add(new AStarNode(calcGCost(start, start), calcHCost(start, end), start, null));
+        while (!nodesToVisit.isEmpty())
+        {
+            AStarNode current = nodesToVisit.pop();
+            if (current.getPoint().equals(end))
+            {
+                return reconstructPath(current);
+            }
+
+            visitedNodes.add(current);
+            for (AStarNode adjacent : getAdjacentNodes(current))
+            {
+                int tentativeGCost = current.getGCost() + 1;
+                if (visitedNodes.contains(adjacent))
+                {
+                    continue;
+                }
+
+                boolean tentativeIsBetter = false;
+                if (!nodesToVisit.contains(adjacent))
+                {
+                    nodesToVisit.add(adjacent);
+                    tentativeIsBetter = true;
+                }
+                else
+                {
+                    tentativeIsBetter = tentativeGCost < adjacent.getGCost();
+                }
+                if (tentativeIsBetter)
+                {
+                    nodesToVisit.remove(adjacent);
+                    adjacent.setPrev(current);
+                    adjacent.setHCost(Math.abs(adjacent.getPoint().getX() - end.getX())
+                            + Math.abs(adjacent.getPoint().getY() - end.getY()));
+                    adjacent.setGCost(tentativeGCost);
+                    nodesToVisit.add(adjacent);
+                    nodes[adjacent.getPoint().getX()][adjacent.getPoint().getY()] = adjacent;
+                }
+            }
+        }
+        return null;
+    }
+
+    public void reset()
+    {
+        nodes = new AStarNode[dims.getX()][dims.getY()];
+    }
+
+    private int calcGCost(Point current, Point pivot)
+    {
+        return Math.abs(current.getX() - pivot.getX()) + Math.abs(current.getY() - pivot.getY());
+    }
+
+    private int calcHCost(Point current, Point pivot)
+    {
+        return Math.abs(current.getX() - pivot.getX()) + Math.abs(current.getY() - pivot.getY());
+    }
+
+    private Set<AStarNode> getAdjacentNodes(AStarNode current)
+    {
+        Set<AStarNode> result = new HashSet<AStarNode>();
+        for (Point disp : DISPS)
+        {
+            int x = current.getPoint().getX() + disp.getX();
+            int y = current.getPoint().getY() + disp.getY();
+            if (x < 0 || x >= dims.getX() || y < 0 || y >= dims.getY())
+            {
+                continue;
+            }
+            if (passable[x][y])
+            {
+                if (nodes[x][y] != null)
+                {
+                    result.add(nodes[x][y]);
+                }
+                else
+                {
+                    result.add(new AStarNode(dims.getX() + dims.getY(), dims.getX() + dims.getY(), new Point(x, y),
+                            current));
+                }
+            }
+        }
+        return result;
+    }
+
+    private LinkedList<Point> reconstructPath(AStarNode current)
+    {
+        LinkedList<Point> result = new LinkedList<Point>();
+        while (current != null)
+        {
+            result.push(current.getPoint());
+            current = current.getPrev();
+        }
+        return result;
+    }
+}
+
+class AStarNode
+{
+    static final class AStarNodeComparator implements Comparator<AStarNode>
+    {
+        @Override
+        public int compare(AStarNode o1, AStarNode o2)
+        {
+            return o1.getFCost() - o2.getFCost();
+        }
+    }
+
+    private int gCost;
+    private int hCost;
+    private final Point point;
+    private AStarNode prev;
+
+    public AStarNode(int gCost, int hCost, Point point, AStarNode prev)
+    {
+        this.gCost = gCost;
+        this.hCost = hCost;
+        this.point = point;
+        this.prev = prev;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (!(o instanceof AStarNode))
+        {
+            return false;
+        }
+        AStarNode node = (AStarNode)o;
+        return point.equals(node.getPoint());
+    }
+
+    public int getFCost()
+    {
+        return gCost + hCost;
+    }
+
+    public int getGCost()
+    {
+        return gCost;
+    }
+
+    public int getHCost()
+    {
+        return hCost;
+    }
+
+    public Point getPoint()
+    {
+        return point;
+    }
+
+    public AStarNode getPrev()
+    {
+        return prev;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return point.hashCode();
+    }
+
+    public void setGCost(int gCost)
+    {
+        this.gCost = gCost;
+    }
+
+    public void setHCost(int hCost)
+    {
+        this.hCost = hCost;
+    }
+
+    public void setPrev(AStarNode prev)
+    {
+        this.prev = prev;
+    }
+
+}
+
+class Point
+{
+    private final int x;
+    private final int y;
+
+    public Point(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (!(o instanceof Point))
+        {
+            return false;
+        }
+        Point point = (Point)o;
+        return x == point.x && y == point.y;
+    }
+
+    public int getX()
+    {
+        return x;
+    }
+
+    public int getY()
+    {
+        return y;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return x + y;
+    }
+
+    @Override
+    public String toString()
+    {
+        return String.format("[%d, %d]", x, y);
+    }
+
 }
--- tests/SolutionTest.java
+++ tests/SolutionTest.java
@@ -2,7 +2,9 @@
  * 
  */
 
+import java.util.List;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -15,6 +17,90 @@ public class SolutionTest
     @Test
     public void test()
     {
+        boolean[][] passable = new boolean[][] { new boolean[] { true }, new boolean[] { true } };
 
+        AStarMap map = new AStarMap(passable);
+        List<Point> path = map.findPath(new Point(0, 0), new Point(1, 0));
+        Assert.assertEquals(2, path.size());
+        Assert.assertEquals(new Point(0, 0), path.get(0));
+        Assert.assertEquals(new Point(1, 0), path.get(1));
+    }
+
+    @Test
+    public void test11()
+    {
+        //@formatter:off
+        boolean[][] passable = new boolean[][] { 
+                { true, true, true},
+                { true, true, true}, 
+                { true, false, true}};
+        //@formatter:on
+
+        Point result = Solution.findAnswer(passable, new Point(0, 0), new Point(2, 2));
+        Assert.assertEquals(new Point(1, 2), result);
+    }
+
+    @Test
+    public void test12()
+    {
+        //@formatter:off
+        boolean[][] passable = new boolean[][] { 
+                { true, true, false},
+                { true, true, true}, 
+                { false, true, true}};
+        //@formatter:on
+
+        Point result = Solution.findAnswer(passable, new Point(0, 0), new Point(2, 2));
+        Assert.assertEquals(new Point(1, 1), result);
+    }
+
+    @Test
+    public void test13()
+    {
+        //@formatter:off
+        boolean[][] passable = new boolean[][] { 
+                { true, true, true},
+                { true, false, true}, 
+                { true, true, true}};
+        //@formatter:on
+
+        Point result = Solution.findAnswer(passable, new Point(0, 0), new Point(0, 2));
+        Assert.assertEquals(new Point(0, 1), result);
+    }
+
+    @Test
+    public void test2()
+    {
+        boolean[][] passable = new boolean[][] { new boolean[] { true, true, true },
+                new boolean[] { true, false, true }, new boolean[] { true, true, true } };
+
+        AStarMap map = new AStarMap(passable);
+        List<Point> path = map.findPath(new Point(0, 0), new Point(1, 2));
+        Assert.assertEquals(4, path.size());
+        Assert.assertEquals(new Point(0, 0), path.get(0));
+        Assert.assertEquals(new Point(0, 1), path.get(1));
+        Assert.assertEquals(new Point(0, 2), path.get(2));
+        Assert.assertEquals(new Point(1, 2), path.get(3));
+    }
+
+    @Test
+    public void test3()
+    {
+        boolean[][] passable = new boolean[][] { new boolean[] { true, true, true, true, true },
+                new boolean[] { true, false, false, false, true }, new boolean[] { true, false, true, true, true },
+                new boolean[] { true, false, false, false, true }, { true, true, true, true, true } };
+
+        AStarMap map = new AStarMap(passable);
+        List<Point> path = map.findPath(new Point(0, 0), new Point(2, 2));
+        Assert.assertEquals(9, path.size());
+        Assert.assertEquals(new Point(0, 0), path.get(0));
+        Assert.assertEquals(new Point(0, 1), path.get(1));
+        Assert.assertEquals(new Point(0, 2), path.get(2));
+        Assert.assertEquals(new Point(0, 3), path.get(3));
+        Assert.assertEquals(new Point(0, 4), path.get(4));
+        Assert.assertEquals(new Point(1, 4), path.get(5));
+        Assert.assertEquals(new Point(2, 4), path.get(6));
+        Assert.assertEquals(new Point(2, 3), path.get(7));
+        Assert.assertEquals(new Point(2, 2), path.get(8));
     }
 }
